3c131d6fafd439a98501ad95e688de5beff3c103
tokiya-takai <t.orca.dolphin@gmail.com>
2021-05-09 02:36:04 +0900

    File download system

--- backend/app/Http/Controllers/HomeController.php
+++ backend/app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Lib\FileProcessing;
 
 class HomeController extends Controller
 {
@@ -12,6 +14,20 @@ class HomeController extends Controller
 
     public function download(Request $request){
         $textData = $request->text_data;
-        var_dump($textData);
+
+        $path = 'storage/files/';
+        if( is_writable($path) ) {
+            // 入力された内容でテキストファイルを作成
+            $file_name = date('Y-m-d-H-i-s') . $this->random(12);
+            file_put_contents( $path . $file_name . ".txt", $textData);
+
+            // ファイルをダウンロード
+            return Storage::download('public/files/' . $file_name . ".txt");
+        }
+    }
+
+    private function random($length)
+    {
+        return substr(str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz'), 0, $length);
     }
 }
